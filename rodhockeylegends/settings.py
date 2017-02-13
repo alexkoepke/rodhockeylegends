376412be272b95bb052831c8af11106536e5a301
Alex Koepke <alexkoepke@users.noreply.github.com>
2017-02-12 23:02:47 -0500

    add heroku ip?

--- rodhockeylegends/settings.py
+++ rodhockeylegends/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["RHL_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "192.180.17.110"]
 
 
 # Application definition
