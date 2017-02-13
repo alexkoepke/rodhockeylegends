85dfead8ecd32a728b3b48ff9d7b39391aa122d4
Alex Koepke <alexkoepke@users.noreply.github.com>
2017-02-12 22:29:14 -0500

    update .gitignore, move some files around

---
+++ puck/static/puck/config.rb
@@ -0,0 +1,17 @@
+## Path to assets
+http_path = "/rodhockeylegends/puck/static"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+
+## Enable relative assets
+relative_assets = true
+
+## Output style
+output_style = :compact #:expanded / :nested / :compact / :compressed
+environment = :development # :production / :development
+
+## SASS Options
+line_comments = true
